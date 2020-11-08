5f076945d05e338adca7db290dbb0d590970257d
lukein77 <lukein77@gmail.com>
2020-11-08 16:58:17 -0300

    arregle errores con la traduccion

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# config/initializers/locale.rb
+ 
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+ 
+# Permitted locales available for the application
+I18n.available_locales = [:en, :es]
+ 
+# Set default locale to something other than :en
+I18n.default_locale = :es
+
 module Combi19
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
