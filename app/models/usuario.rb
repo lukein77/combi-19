037eb901c5c65c4522ef26321e0ffc36c3be5ac3
Juan Cruz Keane <keane221298@gmail.com>
2020-11-16 12:36:46 -0300

    rol default

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,4 +3,14 @@ class Usuario < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize :default_values, unless: :persisted?
+
+  before_save :default_values
+  def default_values
+  	if self.rol == nil 
+    	self.rol = "cliente"
+    end
+  end
+
 end
