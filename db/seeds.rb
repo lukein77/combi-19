ee1e9ca565fefd74d1f16e00ba49f50b3fdf1515
lukein77 <lukein77@gmail.com>
2020-11-17 19:27:51 -0300

    agregue seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Usuario.create( nombre: "Nombre", 
+                apellido: "Apellido",
+                dni: 9999,
+                email: "admin@admin.com",
+                fecha_nacimiento: Date.new(1960,10,30),
+                password: "asdasd")
+
+Usuario.create( nombre: "Nombre", 
+                apellido: "Apellido",
+                dni: 9998,
+                email: "chofer@chofer.com",
+                fecha_nacimiento: Date.new(1960,10,30),
+                password: "asdasd")
+
+Usuario.create( nombre: "Nombre",
+                apellido: "Apellido",
+                dni: 9997,
+                email: "cliente@cliente.com",
+                fecha_nacimiento: Date.new(1960,10,30),
+                password: "asdasd")
