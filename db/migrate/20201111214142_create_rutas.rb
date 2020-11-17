00ceea0de4adfddb2623fd0ff1a915ec9627f9bb
alanwech <alan.wech@hotmail.com>
2020-11-17 19:07:46 -0300

    Checkbox de Adicionales en Ruta

--- db/migrate/20201111214142_create_rutas.rb
+++ db/migrate/20201111214142_create_rutas.rb
@@ -3,7 +3,6 @@ class CreateRutas < ActiveRecord::Migration[6.0]
     create_table :rutas do |t|
       t.string :ciudadOrigen
       t.string :ciudadDestino
-      t.string :nombre
       t.float :tiempo
 
       t.timestamps
