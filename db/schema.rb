36e149f58109ffa4ba725c121e257a0d6f96bff7
lukein77 <lukein77@gmail.com>
2020-11-17 21:05:23 -0300

    relacion entre rutas y adicionales, y header de combi-19

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_233123) do
+ActiveRecord::Schema.define(version: 2020_11_18_000151) do
 
   create_table "adicionales", force: :cascade do |t|
     t.string "nombre"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_11_16_233123) do
     t.float "precio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "adicionales_rutas", force: :cascade do |t|
+    t.integer "adicional_id"
+    t.integer "ruta_id"
+    t.index ["adicional_id"], name: "index_adicionales_rutas_on_adicional_id"
+    t.index ["ruta_id"], name: "index_adicionales_rutas_on_ruta_id"
   end
 
   create_table "ciudades", force: :cascade do |t|
@@ -62,10 +69,10 @@ ActiveRecord::Schema.define(version: 2020_11_16_233123) do
   create_table "rutas", force: :cascade do |t|
     t.string "ciudadOrigen"
     t.string "ciudadDestino"
-    t.string "nombre"
     t.float "tiempo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nombre"
   end
 
   create_table "usuarios", force: :cascade do |t|
@@ -81,6 +88,8 @@ ActiveRecord::Schema.define(version: 2020_11_16_233123) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
   create_table "viajes", force: :cascade do |t|
