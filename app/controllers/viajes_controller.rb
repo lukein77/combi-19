56cb6256d5f5f69e64c3cfc746a681eb8f595957
Juan Cruz Keane <keane221298@gmail.com>
2020-11-20 10:38:02 -0300

    Datetime

--- app/controllers/viajes_controller.rb
+++ app/controllers/viajes_controller.rb
@@ -17,7 +17,7 @@ class ViajesController < ApplicationController
     @combis = Combi.all
     @choferes = Usuario.where(rol: "chofer").where(borrado: false)
     @viaje = Viaje.new(params.require(:viaje).permit(:ruta_id, :combi_id, :chofer_id, :precio, :fecha_hora))
-    if @viaje.fecha_hora - Time.now > 3600
+    if @viaje.fecha_hora - Time.now > 1.days
       if @viaje.save
         redirect_to viajes_path, notice: "El viaje fue creado"
       else
