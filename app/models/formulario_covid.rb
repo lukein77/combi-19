082c0c531ba21a44b3c247284f55ec4e38bbc9c1
lukein77 <lukein77@gmail.com>
2020-12-22 12:40:57 -0300

    cambio en aceptar/rechazar pasajero

--- app/models/formulario_covid.rb
+++ app/models/formulario_covid.rb
@@ -4,4 +4,29 @@ class FormularioCovid < ApplicationRecord
     def actualizado
         updated_at > 48.hours.ago.to_datetime
     end
+
+    def sin_sintomas
+        fiebre < 37 and
+        !perdida_olfato and
+        !perdida_gusto and
+        !tos and
+        !dolor_garganta and 
+        !dificultad_respiratoria and 
+        !dolor_cabeza and
+        !diarrea and
+        !vomitos and
+        !dolor_muscular
+    end
+
+    def sin_antecedentes
+        !convive_con_positivo and
+        !estuvo_con_positivo and
+        !cancer and
+        !diabetes and
+        !enfermedad_hepatica and
+        !enfermedad_renal and
+        !enfermedad_respiratoria and
+        !enfermedad_cardiologica and
+        !bajas_defensas
+    end
 end
